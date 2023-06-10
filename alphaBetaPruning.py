f1009c4c49f7eb47d1d3cc9edea5e2510fcf8375
Hisham Yakan AbuBakr AbdElfatah <hisham.yakan12@gmail.com>
2023-06-10 18:20:03 +0300

    Added The MinMax Class, AlphaBeta Class, Strategy Class

--- alphaBetaPruning.py
+++ alphaBetaPruning.py
@@ -1,3 +1,16 @@
+##############################################################################################################################
+# Project Name :  Othello                                                                                                    #   
+# File Name    :  alphaBetaPruning.py                                                                                        #
+# Author       :  Hisham Yakan                                                                                               #
+# Description  :  This file contains the class that implements the MinMax algorithm. The class inherits from the Strategy    #
+#                interface.                                                                                                  #
+# Date         :  05/25/2023                                                                                                 #
+#                                                                                                                            #                                 
+##############################################################################################################################
+
+
+
+
 # We need to implement the alphabeta pruning algorithm to determine the best move for the computer.
 # The alphabeta pruning algorithm is a recursive algorithm that is used to choose an optimal move for a player assuming that the opponent is also playing optimally.
 # The algorithm searches the game tree starting from the current state of the game and explores all the way down to the terminal states (i.e. states where the game is over).
@@ -11,141 +24,195 @@
 # The following code is used to test the alphabeta pruning algorithm.
 
 from board import ReversiBoard
+from strategy import Strategy
 import math
 
-# This method is used to evaluate the board for the given player.
-# The evaluation function is the difference between the number of pieces of the given player and the number of pieces of the opponent.
-#function Arguments:
-#board: The current state of the game.
-#player: The player for whom we are calculating the score W or B.
 
-def evaluateBoard(board: ReversiBoard,player):
-    if(player == "W"):
-        return len(board.getLocations("W")) - len(board.getLocations("B"))
-    else:
-        return len(board.getLocations("B")) - len(board.getLocations("W"))
+class AlphaBetaPruningStrategy(Strategy):
     
-# This method is used to get the best move for the given player using alphabeta pruning algorithm.
-def getBestMove(boardToGetBestMove : ReversiBoard,player,depth):
     
-    if (boardToGetBestMove.isGameOver()):
-        return None
+    ##############################################################################################################################    
+    
+    # Method Name: evaluateBoard
+    
+    # Purpose: This method is used to evaluate the board for the given player.
 
-    # Get all the valid moves for the given player.
-    validMoves = boardToGetBestMove.getValidMoves(player)
+    # Method Description:
+    # This method is used to evaluate the board for the given player.
+    # The evaluation function is the difference between the number of pieces of the given player and the number of pieces of the opponent.
     
-    if(validMoves == []):
-        return None
-    # If there are no valid moves, then return None.
-    bestMove = None
-    bestScore = None
+    #function Arguments:
+    #board: The current state of the game.
+    #player: The player for whom we are calculating the score W or B.
     
-    # If there are valid moves, then find the best move using alphabeta pruning algorithm.
+    ##############################################################################################################################    
+    
 
-    for move in validMoves:
+    def __evaluateBoard(board: ReversiBoard,player):
+        if(player == "W"):
+            return len(board.getLocations("W")) - len(board.getLocations("B"))
+        else:
+            return len(board.getLocations("B")) - len(board.getLocations("W"))
         
-
-        newBoard = ReversiBoard()
-        newBoard = boardToGetBestMove.getCopy()
-
-        newBoard.makeMove(player,move[0],move[1])
-
-
-        # print("Move: ",move)
-        score = alphaBetaPruning(newBoard,player,depth-1,False,-math.inf,math.inf)
-        # print("Score: ",score)
-        if(bestScore == None or score > bestScore):
-            bestScore = score
-            bestMove = move
-    return bestMove
-
-
-# This method is used to implement the alphabeta pruning algorithm.
-# function Arguments:
-# board: The current state of the game.
-# player: The player for whom we are calculating the score W or B.
-# depth: The depth of the game tree.
-# isMaximizingPlayer: A boolean value that indicates whether the current node is a maximizing node or not.
-# alpha: The best value that the maximizing player can guarantee at that level or above.
-# beta: The best value that the minimizing player can guarantee at that level or above.
-def alphaBetaPruning(board :ReversiBoard,player,depth,isMaximizingPlayer,alpha,beta):
+    ##############################################################################################################################
+        
+        
+    ##############################################################################################################################    
+    # Method Name: getBestMove
     
-    # If the depth is 0 or the game is over, then return the score.
-    if(depth == 0 or board.isGameOver()):
-        return evaluateBoard(board,player)
+    # Purpose: This method is used to get the best move for the given player using alphabeta pruning algorithm.
     
-    # Get all the valid moves for the given player.
-    validMoves = board.getValidMoves(player)
+    # Method Description:    
+    # This method is used to get the best move for the given player using alphabeta pruning algorithm.
     
-    # If there are no valid moves, then return the score.
-    if(validMoves == []):
-        return alphaBetaPruning(board , board.getOpponent(player) , depth , not isMaximizingPlayer , alpha , beta)
+    #function Arguments:
+    #boardToGetBestMove: The current state of the game.
+    #player: The player for whom we are calculating the score W or B.
+    #depth: The depth of the game tree.
     
-    # If the current node is a maximizing node.
-    if(isMaximizingPlayer):
-        bestScore = -math.inf
+    ##############################################################################################################################    
+    
+    
+    def getBestMove(boardToGetBestMove : ReversiBoard,player,depth):
+        
+        # If the game is over, then return None.
+        if (boardToGetBestMove.isGameOver()):
+            return None
+
+        # Get all the valid moves for the given player.
+        validMoves = boardToGetBestMove.getValidMoves(player)
+        
+        # If there are no valid moves, then return None.
+        if(validMoves == []):
+            return None
+        # If there are no valid moves, then return None.
+        bestMove = None
+        bestScore = None
+        
+        # If there are valid moves, then find the best move using alphabeta pruning algorithm.
+        
+        # For each valid move, create a new board and make the move on the new board.
         for move in validMoves:
-            newBoard = ReversiBoard()
-            newBoard = board.getCopy()
-            newBoard.makeMove(player,move[0],move[1])
-            score = alphaBetaPruning(newBoard,player,depth-1,False,alpha,beta)
-            bestScore = max(bestScore,score)
-            alpha = max(alpha,score)
-            if(beta <= alpha):
-                break
-        return bestScore
-    # If the current node is a minimizing node.
-    else:
-        bestScore = math.inf
-        for move in validMoves:
-            newBoard = ReversiBoard()
-            newBoard = board.getCopy()
-            newBoard.makeMove(player,move[0],move[1])
-            score = alphaBetaPruning(newBoard,player,depth-1,True,alpha,beta)
-            bestScore = min(bestScore,score)
-            beta = min(beta,score)
-            if(beta <= alpha):
-                break
-        return bestScore
-    
-    
-    
-import concurrent.futures
-
-def getBestMoveWithMT(boardToGetBestMove: ReversiBoard, player, depth):
-    
-    if boardToGetBestMove.isGameOver():
-        return None
-
-    # Get all the valid moves for the given player.
-    validMoves = boardToGetBestMove.getValidMoves(player)
-    
-    if not validMoves:
-        return None
-
-    bestMove = None
-    bestScore = None
-
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        futures = []
-
-        for move in validMoves:
+            
+            # Create a new board
             newBoard = ReversiBoard()
             newBoard = boardToGetBestMove.getCopy()
-            newBoard.makeMove(player, move[0], move[1])
 
-            # Submit each move computation as a separate thread
-            futures.append(executor.submit(alphaBetaPruning, newBoard, player, depth - 1, False, -1000000, 1000000))
+            # Make the move on the new board.
+            newBoard.makeMove(player,move[0],move[1])
 
-        # Retrieve the results from the completed threads
-        for move, future in zip(validMoves, futures):
-            score = future.result()
 
-            if bestScore is None or score > bestScore:
+            # Call the alphabeta pruning algorithm to get the score for the move (The heuristics ).               
+            score = AlphaBetaPruningStrategy.alphaBetaPruning(newBoard,player,depth-1,False,-math.inf,math.inf)
+            
+            # If the score is better than the best score, then update the best score and the best move.
+            if(bestScore == None or score > bestScore):
                 bestScore = score
                 bestMove = move
-
-    return bestMove
-
+        
+        # Return the best move.        
+        return bestMove
+    
+    ##############################################################################################################################
     
     
+
+    ##############################################################################################################################    
+    # Method Name: alphaBetaPruning
+    
+    # Purpose: This method is used to implement the alphabeta pruning algorithm.
+    
+    # Method Description:
+    # This method is used to implement the alphabeta pruning algorithm to be used to determine the best move.
+    
+    # function Arguments:
+    # board: The current state of the game.
+    # player: The player for whom we are calculating the score W or B.
+    # depth: The depth of the game tree.
+    # isMaximizingPlayer: A boolean value that indicates whether the current node is a maximizing node or not.
+    # alpha: The best value that the maximizing player can guarantee at that level or above.
+    # beta: The best value that the minimizing player can guarantee at that level or above.
+    
+    ##############################################################################################################################
+    
+    def alphaBetaPruning(board :ReversiBoard,player,depth,isMaximizingPlayer,alpha,beta):
+        
+        # If the depth is 0 or the game is over, then return the score.
+        if(depth == 0 or board.isGameOver()):
+            return AlphaBetaPruningStrategy.__evaluateBoard(board,player)
+        
+        # Get all the valid moves for the given player.
+        validMoves = board.getValidMoves(player)
+        
+        # If there are no valid moves, then return the score.
+        if(validMoves == []):
+            return AlphaBetaPruningStrategy.alphaBetaPruning(board , board.getOpponent(player) , depth , not isMaximizingPlayer , alpha , beta)
+        
+        # If the current node is a maximizing node.
+        if(isMaximizingPlayer):
+            
+            # Initialize the best score of alpha to -infinity.
+            bestScore = -math.inf
+            
+            # Loop through all the valid moves.
+            for move in validMoves:
+                
+                # Create a new board.
+                newBoard = ReversiBoard()
+                newBoard = board.getCopy()
+                
+                # Make the move on the new board.
+                newBoard.makeMove(player,move[0],move[1])
+                
+                # Call the alphabeta pruning algorithm to get the score for the move (The heuristics ).
+                score = AlphaBetaPruningStrategy.alphaBetaPruning(newBoard,player,depth-1,False,alpha,beta)
+                
+                # Update the best score.
+                bestScore = max(bestScore,score)
+                
+                # Update the alpha value.
+                alpha = max(alpha,score)
+                
+                # If alpha >= beta, then prune the branch.
+                if(beta <= alpha):
+                    break
+             
+            # Return the best score.    
+            return bestScore
+        
+        
+        # If the current node is a minimizing node.
+        else:
+            # Initialize the best score of beta to infinity.
+            bestScore = math.inf
+            
+            # Loop through all the valid moves.
+            for move in validMoves:
+                
+                # Create a new board.
+                newBoard = ReversiBoard()
+                newBoard = board.getCopy()
+                
+                # Make the move on the new board.
+                newBoard.makeMove(player,move[0],move[1])
+                
+                # Call the alphabeta pruning algorithm to get the score for the move (The heuristics ).
+                score = AlphaBetaPruningStrategy.alphaBetaPruning(newBoard,player,depth-1,True,alpha,beta)
+                
+                # Update the best score to the minimum of the current best score and the score.
+                bestScore = min(bestScore,score)
+                
+                # Update the beta value to the minimum of the current beta value and the score.
+                beta = min(beta,score)
+                
+                # If alpha >= beta, then prune the branch.
+                if(beta <= alpha):
+                    break
+                
+            # Return the best score.
+            return bestScore
+        
+        
+        
+    
+    ##############################################################################################################################
