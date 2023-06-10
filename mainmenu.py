4ca4f0f00b073186e873b6952bd25906147ec61c
Moaaz Hamdy <52506331+Mo3az2000@users.noreply.github.com>
2023-06-10 17:19:19 +0300

    made the font Dynamic

--- mainmenu.py
+++ mainmenu.py
@@ -12,27 +12,27 @@ class MainMenu:
             self.game_object.screen_height,
             theme=pygame_menu.themes.THEME_SOLARIZED)
         # set menu font size
+        self.font_size = 28 * self.game_object.screen_width // 480
 
         # Add game mode selection
         menu.add.selector('Game Mode: ', [
             ('Player VS Player', 'VS Player'),
             ('Player VS AI', 'VS AI'),
             ('AI VS AI', 'AI VS AI')],
-            onchange=self.game_object.set_game_mode,
-            font_size=18)
+            onchange=self.game_object.set_game_mode, font_size=self.font_size)
 
         # Add difficulty selection
         menu.add.selector('Difficulty: ', [('Easy', 'easy'), ('Medium', 'medium'), ('Hard', 'hard')],
                           onchange=self.game_object.set_difficulty,
-                          font_size=18)
+                          font_size=self.font_size)
 
         # Add start button
         menu.add.button('Start', self.game_object.start_game,
-                        font_size=18)
+                        font_size=self.font_size)
 
         # Add quit button
         menu.add.button('Quit', pygame_menu.events.EXIT,
-                        font_size=18)
+                        font_size=self.font_size)
 
         menu.mainloop(self.game_object.screen)
 
