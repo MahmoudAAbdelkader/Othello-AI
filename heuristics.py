7e0041b7cf6cd9b7a1f2f4a99373297fba7c1ad1
NadaAmgadSayed <engnadaamged@gmail.com>
2023-06-10 22:20:19 +0300

    Revert "Commit The coinParity function"
    This reverts commit 09d37b5bb8a6ce1891d2d9c5a95aaeb260b51f67.

--- heuristics.py
+++ heuristics.py
@@ -3,8 +3,8 @@
 # Authors: Nada Amgad , Nada Youssef
 # Date: 2022-6-10
 # Description: This file contains all the heuristics that will be used in the game.
-##############################################################################################################
-from board import boardobj
+############################################################################################################
+
 board = [
     [" ", " ", " ", " ", " ", " ", " ", " "],
     [" ", " ", " ", " ", " ", " ", " ", " "],
@@ -18,22 +18,12 @@ board = [
 
 class GameHeuristics():
     def _init_(self):
-       pass
+        pass
+
 
     def coinParity(self):
-        for row in board:
-            for item in row:
-                if (item == "B"):
-                    B_coins = B_coins+1
-                elif (item == "W"):
-                    W_coins = W_coins+1
+        print('in progress')
 
-        if(B_coins > W_coins):
-            coinParityWeight = 100 * ((B_coins - W_coins) / (B_coins + W_coins))
-        else:
-            coinParityWeight = 100*((W_coins-B_coins)/(W_coins+B_coins))
-
-        return coinParityWeight
 
     def mobility(self):
         print('in progress')
