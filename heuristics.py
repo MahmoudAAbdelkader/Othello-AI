763de735700010b47cca7d99952108974c48ab64
NadaAmgadSayed <engnadaamged@gmail.com>
2023-06-10 15:33:56 +0300

    intial commit - heuristics class (in progress)

---
+++ heuristics.py
@@ -0,0 +1,41 @@
+###############################################################################################################
+# File: heuristics.py
+# Authors: Nada Amgad , Nada Youssef
+# Date: 2022-6-10
+# Description: This file contains all the heuristics that will be used in the game.
+############################################################################################################
+
+board = [
+    [" ", " ", " ", " ", " ", " ", " ", " "],
+    [" ", " ", " ", " ", " ", " ", " ", " "],
+    [" ", " ", " ", " ", " ", " ", " ", " "],
+    [" ", " ", " ", "W", "B", " ", " ", " "],
+    [" ", " ", " ", "B", "W", " ", " ", " "],
+    [" ", " ", " ", " ", " ", " ", " ", " "],
+    [" ", " ", " ", " ", " ", " ", " ", " "],
+    [" ", " ", " ", " ", " ", " ", " ", " "]
+]
+
+class GameHeuristics():
+    def _init_(self):
+        pass
+
+
+    def coinParity(self):
+        print('in progress')
+
+
+    def mobility(self):
+        print('in progress')
+
+
+    def cornersCaptured(self):
+        print('in progress')
+
+
+    def stability(self):
+        print('in progress')
+
+
+    def combinedHeuristics(self):
+        print('in progress')
