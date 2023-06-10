1262fd17953b0b872d5fb669e77fdbc0d72e8849
MahmoudAAbdelkader <alameldeenmahmoud@gmail.com>
2023-06-10 15:41:46 +0300

    moving player to separate file

--- othelloUI.py
+++ othelloUI.py
@@ -1,46 +1,11 @@
 import pygame
 import sys
 import pygame_menu
+from player import AIPlayer, HumanPlayer
 from reversi2 import ReversiBoard
 
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
-
-
-### Player Classes ###
-
-class Player:
-    def __init__(self, color):
-        self.color = color
-        self.score = 2  # initial score
-        self.is_turn = False
-
-    def get_color(self):
-        return self.color
-
-    def update_score(self, score):
-        self.score += score
-
-    def switch_turn(self):
-        self.is_turn = not self.is_turn
-
-
-class AIPlayer(Player):
-    def choose_move(self, game_board):
-        # TODO: Implement the logic for choosing a move
-        return (0, 0)  # Return a dummy move for now
-
-
-class HumanPlayer(Player):
-    def get_input(self):
-        for event in pygame.event.get():
-            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-                x, y = event.pos
-                col = x // self.square_size
-                row = y // self.square_size
-                if 0 <= row < 8 and 0 <= col < 8:
-                    return (row, col)
-        return None
 
 ### Board Class ###
 
